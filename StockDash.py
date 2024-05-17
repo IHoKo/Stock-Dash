f8e04912f54c7d8d50b1940d239a78d3cd874d8c
IHoKo <hsn.kor@gmail.com>
2024-05-17 17:13:10 -0400

    change the background color

--- StockDash.py
+++ StockDash.py
@@ -2,6 +2,7 @@ import dash
 from dash import dcc,html
 from dash.dependencies import Input, Output, State
 import pandas_datareader.data as web
+import plotly.graph_objs as go
 from datetime import datetime
 import pandas as pd
 import yfinance as yf
@@ -15,6 +16,18 @@ options = []
 for tic in nsdq.index:
     options.append({'label':'{} {}'.format(tic,nsdq.loc[tic]['Name']), 'value':tic})
 
+light_theme = {
+    "main-background": "#ffe7a6",
+    "header-text": "#376e00",
+    "sub-text": "#0c5703",
+}
+
+dark_theme = {
+    "main-background": "#000000",
+    "header-text": "#ff7575",
+    "sub-text": "#ffd175",
+}
+
 app.layout = html.Div([
     html.H1('Stock Ticker Dashboard'),
     html.Div([
@@ -27,7 +40,9 @@ app.layout = html.Div([
             multi=True
         )
     # widen the Div to fit multiple inputs
-    ], style={'display':'inline-block', 'verticalAlign':'top', 'width':'30%'}),
+    ], style={'display':'inline-block', 
+    'verticalAlign':'top', 
+    'width':'30%',}),
     html.Div([
         html.H3('Select start and end dates:'),
         dcc.DatePickerRange(
@@ -50,11 +65,11 @@ app.layout = html.Div([
         id='my_graph',
         figure={
             'data': [
-                {'x': [1,2], 'y': [3,1]}
+                {'x': [1,2], 'y': [1,2]}
             ]
         }
     )
-])
+], style={'backgroundColor':'#1E1E1E', 'color': '#FFFFFF'},)
 @app.callback(
     Output('my_graph', 'figure'),
     [Input('submit-button', 'n_clicks')],
@@ -66,17 +81,31 @@ def update_graph(n_clicks, stock_ticker, start_date, end_date):
     end = datetime.strptime(end_date[:10], '%Y-%m-%d')
     print('*********************************',start,end)
     # since stock_ticker is now a list of symbols, create a list of traces
-    traces = []
+    # traces = []
+    fig = go.Figure()
+
     for tic in stock_ticker:
         # df = web.DataReader(tic,'iex',start,end)
         df = web.get_data_yahoo(tic, start=start, end=end)
-        traces.append({'x':df.index, 'y': df.Close, 'name':tic})
-    fig = {
-        # set data equal to traces
-        'data': traces,
-        # use string formatting to include all symbols in the chart title
-        'layout': {'title':', '.join(stock_ticker)+' Closing Prices'}
-    }
+        fig.add_trace(go.Scatter(
+        x=df.index,
+        y=df.Close,
+        mode='lines',
+        showlegend=False
+    ))
+        # traces.append({'x':df.index, 'y': df.Close, 'name':tic})
+    # fig = {
+    #     # set data equal to traces
+    #     'data': traces,
+    #     # use string formatting to include all symbols in the chart title
+    #     'layout': {'title':', '.join(stock_ticker)+' Closing Prices', 'template':'plotly_dark'}
+    # }
+        fig.update_layout(
+        title=', '.join(stock_ticker)+' Closing Prices',
+        plot_bgcolor='#1E1E1E',
+        paper_bgcolor='#1E1E1E',
+        font=dict(color='#FFFFFF')
+    )
     return fig
 
 if __name__ == '__main__':
